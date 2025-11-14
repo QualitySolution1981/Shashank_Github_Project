4eb9254c380e5a809ed25a18be3c5b07e6142ceb
testingqualitysolution19-admin <testingqualitysolution+a100@gmail.com>
2025-11-14 13:04:44 +0530

    new class add

---
+++ Shashank_Github_Project/src/ShashankPCKG/Karan.java
@@ -0,0 +1,38 @@
+package ShashankPCKG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Karan {
+ 
+	 @BeforeTest
+	  public void beforeTest() {
+	  }
+	
+	
+	@Test
+  public void bothvalue() {
+  }
+	
+
+	@Test
+  public void sc() {
+  }
+	
+
+	@Test
+  public void sdfsf() {
+  }
+	
+
+	@Test
+  public void sdfssf() {
+  }
+ 
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
